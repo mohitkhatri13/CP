162c8fb699d1c4d17894387d842f5e15921115f1
mohit khatri <khatrimohit2002@gmail.com>
2025-09-24 23:41:55 +0530

    string to int ll

---
+++ DSA/string to int.cpp
@@ -0,0 +1,19 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   string temp = "9223372036854775807";
+    long long v = stoll(temp);
+   cout<<v<<endl;
+   
+   long long c = 9223372036854775807;
+//    use c = 9223372036854775807LL   LL at last for better 
+   string tep = to_string(c);
+   cout<<tep<<endl;
+   
+
+    return 0;
+}
