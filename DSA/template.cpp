e9c7bfec919c7cfb2e7254de9cfe8cee6c08775f
mohit khatri <khatrimohit2002@gmail.com>
2025-10-13 02:09:32 +0530

    template

---
+++ DSA/template.cpp
@@ -0,0 +1,24 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define init int a; cin>>a; while(a--)
+#define endl "\n"
+
+#define initvector(name, n) int n; cin >> n; vector<int> name(n); for(int i = 0; i < n; i++) cin >> name[i];
+
+
+int32_t main()
+{
+    init{
+        initvector(mohit , n);
+        
+        for( auto it : mohit){
+            cout<<it<<" ";
+        }
+        
+        
+    }
+
+    return 0;
+}
